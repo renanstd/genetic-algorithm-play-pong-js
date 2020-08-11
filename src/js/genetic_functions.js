d8e87b4851951928e7feb56dc13f820dc13dc940
Renan Santana <renan.santana@instruct.com.br>
2020-08-11 13:26:57 -0300

    criacao de parametros de ga

--- src/js/genetic_functions.js
+++ src/js/genetic_functions.js
@@ -1,4 +1,11 @@
-function create_population() {
+// Variables --------------------------------------------------------
+const population_size = 10;
+const selection_method = 'roulette_wheel';
+const crossover_probability = 0.9; // 90%
+const mutation_probability = 0.01; // 01%
+
+
+function create_initial_population() {
 
 }
 
